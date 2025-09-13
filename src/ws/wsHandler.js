4fb6c4998bb372b1f7b2cc4e313848cb85c992f0
ritik kumar <kr.ritik1510@gmail.com>
2025-09-13 07:01:11 +0530

    fix: improve modularity

--- src/ws/wsHandler.js
+++ src/ws/wsHandler.js
@@ -1,18 +1,21 @@
-import { messagesHandler } from "../messagesHandlers/Message.handlers.js";
+import { messagesHandler } from "../connectionHandlers/Message.handlers.js";
 
-// WebSocket handler module
+/**
+ * webSocket handler module
+ * notify client that connection is established
+ * listen for messages from client
+ * handle client closing connection
+ */
+
 export function wsHandler(ws) {
   console.log("✅ Client connected");
-
-  // Notify client that connection is established
   ws.send(
-    JSON.stringify({ type: "connection", message: "Connection established" })
+    JSON.stringify({
+      type: "connection",
+      essage: "Connection established"
+    })
   );
-
-  // Listen for messages from client
   ws.on("message", (msg) => messagesHandler(ws, msg));
-
-  // Handle client closing connection
   ws.on("close", () => {
     console.log("❌ Client disconnected");
   });
